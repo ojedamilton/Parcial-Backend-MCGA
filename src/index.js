1e4bd800a11c58f705fa8c2d7f7ee39bd3038d4e
Milton Ojeda <https://github.com/ojedamilton>
2022-09-29 23:14:17 -0300

    MCGA-02: Conection mongoose

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
+dotenv.config();
+// Middleware Json
+app.use(express.json());
+// Assign Port
+app.listen(process.env.PORT,()=> console.log(`server on port 3000`));
+
+mongoose.connect(process.env.CONECTION_URL)
+  .then(() => {
+    console.log("🟢 DB Connected");
+  })
+ .catch((err) => {
+    console.log("🔴 There was an error on the DB connection method.");
+    console.log(err);
+}); 
