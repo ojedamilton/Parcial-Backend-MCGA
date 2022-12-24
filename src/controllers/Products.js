7beb0a56840ca7aa563d985ccbb4f2612f6a329f
Ojeda Milton Ezequiel <ojedamiltonezequiel@gmail.com>
2022-12-23 22:48:00 -0300

    MCGA-07 : Cors

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -1,52 +1,62 @@
 const Products = require('../models/Products');
 
-//  CREATE PRODUCTS
-const createProduct = (req, res) => {
-    const {_id,name,price,stock,description}=req.body;  
-    const newProvider = {_id,name,price,stock,description};
-    Products.create(newProvider)    
-      .then((data) => res.status(201).json({ msg: "Products added: ", data , error:false }))
-      .catch((err) => res.status(500).json({ msg: `Error: ${err}`,data:{},error:true }));
-  };
-
-// GET ALL PRODUCTS
+//Get All
 const getAll = (req, res) => {
-Products.find({isDeleted:false})
-    .then((data) => res.status(200).json({msg:"All Products",data,error:false }))
-    .catch((err) => res.status(500).json({ msg: `Error: ${err}`,data:{},error:true }));
-}
-// GET BY ID PRODUCT
+    Products.find({isDeleted: false})
+        .then((data) => res.status(200).json({ msg: "All Products", data, error: false }))
+
+        .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
+};
+
+//Get
 const getById = (req, res) => {
     const { _id } = req.params;
     Products.findById(parseInt(_id))
-        .then((data) => { 
-             if(!data) return res.status(404).json({ msg: `Products not found by ID: ${id}`,data:{},error:true });         
-             return res.status(200).json({msg:"Product By Id",data,error:false })
-         })
-        .catch((err) => res.status(500).json({ msg: `Error: ${err}`,data:{},error:true }));
-    }
-// REMOVE PROVIDER
-const removeProduct = (req, res) => {
-const { _id } = req.params;
-Products.findByIdAndUpdate( parseInt(_id), { isDeleted: true }, { new: true })
-    .then((data) => {
-    if (!data) return res.status(404).json({ msg: `Products not found by ID: ${id}`,data:{},error:true });
-    return res.status(202).json({ msg: "Products deleted", data ,error:false});
-    })
-    .catch((err) => res.status(500).json({ msg: `Error: ${err}` ,data:{},error:true}));
+        .then((data) => {
+            if (!data) {
+                return res.status(404).json({msg: "Product not found"})
+            };
+            res.status(200).json({ msg: "Product by Id", data, error: false })
+        })
+        .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
 };
 
-// UPDATE PROVIDER
-const updateProduct= (req, res) => {
-const { _id } = req.params;
-Products.findByIdAndUpdate(parseInt(_id), req.body, { new: true })
-    .then((data) => {
-    if (!data) return res.status(404).json({ msg: `Products not found by ID: ${_id}`,data:{},error:true });
-    return res.status(202).json({ msg: "Products updated", data , error:false });
-    })
-    .catch((err) => res.status(500).json({ msg: `Error: ${err}` ,data:{},error:true }));
+// Post
+const createProduct = (req, res) => {
+    console.log('estoy aca');
+    const {name, price, stock, description } = req.body;
+    const sumLastId= Products.find().sort({$natural:-1}).limit(1)
+                .then((data)=> {
+                    console.log("el stock es :"+data[0].stock + "id: "+ data[0]._id);
+                    const newProduct = { _id:data[0]._id+1, name, price, stock, description };
+                    Products.create(newProduct)
+                        .then((data) => res.status(201).json({ msg: "Product added: ", data, error: false }))
+                        .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));                     
+                    })
+                    .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true })); 
 };
-  // EXPORTS ALL
+
+// Update
+const updateProduct = (req, res) => {
+    const { _id } = req.params;
+    Products.findByIdAndUpdate(parseInt(_id), req.body, { new: true })
+        .then((data) => {
+            if (!data || data.length === 0) return res.status(404).json({ msg: `Product not found by ID: ${_id}`, data: {}, error: true });
+            return res.status(202).json({ msg: "Product updated", data, error: false });
+        })
+        .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
+};
+
+// Delete
+const removeProduct = (req, res) => {
+    const { _id } = req.params;
+    Products.findByIdAndUpdate(parseInt(_id), { isDeleted: true })
+        .then((data) => {
+            if (!data || data.length === 0) return res.status(404).json({ msg: `Product not found by ID: ${id}`, data: {}, error: true });
+            return res.status(202).json({ msg: "Product deleted", data, error: false });
+        })
+        .catch((err) => res.status(500).json({ msg: `Error: ${err}`, data: {}, error: true }));
+};
 
   module.exports = {
     getAll,
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes/Products');
 
 dotenv.config();
 // Middleware Json
 app.use(express.json());
+app.use(cors());
 // Assign Port
-app.listen(process.env.PORT,()=> console.log(`server on port 3000`));
+app.listen(process.env.PORT,()=> console.log(`server on port 4000`));
 
 mongoose.connect(process.env.CONECTION_URL)
   .then(() => {
